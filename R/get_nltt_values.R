6cc0f9b0c17c92691753787254e52bca0277beb5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-10 12:24:57 +0100

    Introduced new function

--- R/get_nltt_values.R
+++ R/get_nltt_values.R
@@ -6,19 +6,6 @@
 #'
 #' @export
 get_nltt_values <- function(phylogenies, dt) {
-  m <- get_nltt_values_unmelted(phylogenies = phylogenies, dt = dt)
-  reshape2::melt(m, id = "time")
-}
-
-
-#' Collects the nLTT values of all phylogenies in the unmelted/cast/short form
-#'
-#' @param phylogenies the phylogenies, supplied as either a list or a multiPhylo object, where the phylogenies are of type 'phylo'
-#' @param dt The timestep resolution, where 1/dt is the number of points evaluated
-#' @return A dataframe of timepoints with the nLTT value of each phylogeny in time
-#'
-#' @export
-get_nltt_values_unmelted <- function(phylogenies, dt) {
   if (length(phylogenies) < 1) {
     stop("get_average_nltt_matrix: ",
          "there must be at least one phylogeny supplied")
@@ -40,11 +27,11 @@ get_nltt_values_unmelted <- function(phylogenies, dt) {
          "dt was ", dt, " instead")
   }
 
-  n_rows <- (1.0 / dt) + 1
+  n_cols <- 3 # ID, t, nLTT(t)
+  n_rows_per_phylogeny <- (1.0 / dt) + 1
   n_phylogenies <- length(phylogenies)
-  n_cols <- n_phylogenies + 1
+  n_rows <- n_rows_per_phylogeny * n_phylogenies
   m <- matrix(nrow = n_rows, ncol = n_cols)
-  m[, 1] <- seq(0,1,dt)
   for (i in seq(1, n_phylogenies)) {
     testit::assert(i >= 1 && i <= n_phylogenies)
     new_col <- ribir::stretch_nltt_matrix(
@@ -52,11 +39,19 @@ get_nltt_values_unmelted <- function(phylogenies, dt) {
       dt = dt,
       step_type = "upper"
     )
-    testit::assert(length(new_col[, 2]) == n_rows)
-    m[, i + 1]  <- new_col[, 2, drop = FALSE]
+    testit::assert(length(new_col[, 2]) == n_rows_per_phylogeny)
+    row_from_index <- (i - 1) * n_rows_per_phylogeny + 1
+    row_to_index <- (i - 0) * n_rows_per_phylogeny
+    testit::assert(row_from_index >= 1)
+    testit::assert(row_from_index <= n_rows)
+    testit::assert(row_to_index >= 1)
+    testit::assert(row_to_index <= n_rows)
+    m[row_from_index:row_to_index, 1] <- i # Recycling
+    m[row_from_index:row_to_index, 2] <- seq(0,1,dt)
+    m[row_from_index:row_to_index, 3] <- new_col[, 2, drop = FALSE]
   }
   z <- as.data.frame(x = m)
-  colnames(z) <- c("time", paste(rep("N", 2),
-    as.character(seq(1, n_phylogenies)), sep = ""))
+  colnames(z) <- c("ID","t","nltt")
+  z[, 1] <- sapply(z[, 1], as.integer)
   z
 }
--- tests/testthat/test_get_nltt_values.R
+++ tests/testthat/test_get_nltt_values.R
@@ -1,8 +1,33 @@
 test_that(paste("get_nltt_values: ",
-  "How to stretch an nLTT timepoints matrix: ",
-  "Example: Easy tree", sep = ""), {
+  "data types", sep = ""), {
 
-  # The average of nLTTs A and B should be C
+  newick1 <- "((A:1,B:1):1,(C:1,D:1):1);"
+  newick2 <- paste("((((XD:1,ZD:1):1,CE:2):1,(FE:2,EE:2):1):4,",
+    "((AE:1,BE:1):1,(WD:1,YD:1):1):5);", sep = "")
+  phylogeny1 <- ape::read.tree(text = newick1)
+  phylogeny2 <- ape::read.tree(text = newick2)
+  phylogenies <- c(phylogeny1, phylogeny2)
+  result <- ribir::get_nltt_values(
+    c(phylogeny1, phylogeny2), dt = 0.20)
+
+
+  expect_equal(nrow(result), 12)
+  expect_equal(ncol(result), 3)
+
+  expect_equal(is.data.frame(result), TRUE)
+  expect_equal(is.integer(result$ID), TRUE)
+  expect_equal(is.factor(result$ID), FALSE)
+  expect_equal(is.numeric(result$t), TRUE)
+  expect_equal(is.numeric(result$nltt), TRUE)
+})
+
+
+
+test_that(paste("get_nltt_values: ",
+  "How to stretch an nltt timepoints matrix: ",
+  "Example: Two easy tree", sep = ""), {
+
+  # The average of nltts A and B should be C
   #
   #      A              B              C       # nolint
   #                                            # nolint
@@ -49,21 +74,32 @@ test_that(paste("get_nltt_values: ",
 
   phylogenies <- c(phylogeny1, phylogeny2)
   testit::assert(length(phylogenies) == 2)
-  result <- ribir::get_nltt_values_unmelted(
+  result <- ribir::get_nltt_values(
     phylogenies, dt = 0.20)
-  ##      [,1]      [,2]       [,3]  # nolint
-  ## [1,]  0.0 0.6666667  0.6666667  # nolint
-  ## [2,]  0.2 0.6666667  0.6666667  # nolint
-  ## [3,]  0.4 0.6666667  1.0000000  # nolint
-  ## [4,]  0.6 0.6666667  1.0000000  # nolint
-  ## [5,]  0.8 1.0000000  1.0000000  # nolint
-  ## [6,]  1.0 1.0000000  1.0000000  # nolint
-  m <- matrix(c(seq(0.0, 1.0, 0.2),
-    rep(2 / 3, 4), rep(1, 2), rep(2 / 3, 2), rep(1, 4)), ncol = 3)
+  ## [1,]  1 0.0 0.6666667  # nolint
+  ## [2,]  1 0.2 0.6666667  # nolint
+  ## [3,]  1 0.4 0.6666667  # nolint
+  ## [4,]  1 0.6 0.6666667  # nolint
+  ## [5,]  1 0.8 1.0000000  # nolint
+  ## [6,]  1 1.0 1.0000000  # nolint
+  ## [1,]  2 0.0 0.6666667  # nolint
+  ## [2,]  2 0.2 0.6666667  # nolint
+  ## [3,]  2 0.4 1.0000000  # nolint
+  ## [4,]  2 0.6 1.0000000  # nolint
+  ## [5,]  2 0.8 1.0000000  # nolint
+  ## [6,]  2 1.0 1.0000000  # nolint
+
+  m <- matrix(
+      c(rep(1, 6), rep(2, 6),
+      rep(seq(0, 1, 0.2), 2),
+      rep(2 / 3, 4), rep(1, 2), rep(2 / 3, 2), rep(1, 4)),
+      ncol = 3, byrow = FALSE
+    )
   expected <- as.data.frame(x = m)
-  colnames(expected) <- c("time", paste(rep("N", 2),
-    as.character(seq(1, n_phylogenies)), sep = ""))
-  expect_equal(nrow(result), 6)
+  colnames(expected) <- c("ID", "t", "nltt")
+  expected[, 1] <- sapply(expected[, 1], as.integer)
+
+  expect_equal(nrow(result), 12)
   expect_equal(ncol(result), 3)
   expect_equal(all.equal(result, expected), TRUE)
 })
@@ -73,8 +109,8 @@ test_that(paste("get_nltt_values: ",
 
 
 test_that(paste("get_nltt_values: ",
-  "How to stretch an nLTT timepoints matrix: ",
-  "Example: Harder trees", sep = ""), {
+  "How to stretch an nltt timepoints matrix: ",
+  "Example: Two harder trees", sep = ""), {
 
   newick1 <- "((A:1,B:1):1,(C:1,D:1):1);"
   newick2 <- paste("((((XD:1,ZD:1):1,CE:2):1,(FE:2,EE:2):1):4,",
@@ -114,21 +150,54 @@ test_that(paste("get_nltt_values: ",
   #phylogenies <- c(phylogeny1, phylogeny2)  # nolint
 
   # The real tests
-  result <- ribir::get_nltt_values_unmelted(
+  result <- ribir::get_nltt_values(
     c(phylogeny1, phylogeny2), dt = 0.20)
 
-  ##      [,1] [,2]  # nolint
-  ## [1,]  0.0  0.5  0.2222222 # nolint
-  ## [2,]  0.2  0.5  0.2222222 # nolint
-  ## [3,]  0.4  0.5  0.2222222 # nolint
-  ## [4,]  0.6  1.0  0.3333333 # nolint
-  ## [5,]  0.8  1.0  0.6666667 # nolint
-  ## [6,]  1.0  1.0  1.0000000 # nolint
-  m <- matrix(c(seq(0.0, 1.0, 0.2),
-    rep(0.5, 3), rep(1.0, 3), rep(2 / 9, 3), 1 / 3, 2 / 3, 1.0), ncol = 3)
+  ## [1,]  1 0.0  0.5  # nolint
+  ## [2,]  1 0.2  0.5  # nolint
+  ## [3,]  1 0.4  0.5  # nolint
+  ## [4,]  1 0.6  1.0  # nolint
+  ## [5,]  1 0.8  1.0  # nolint
+  ## [6,]  1 1.0  1.0  # nolint
+  ## [1,]  2 0.0 0.2222222  # nolint
+  ## [2,]  2 0.2 0.2222222  # nolint
+  ## [3,]  2 0.4 0.2222222  # nolint
+  ## [4,]  2 0.6 0.3333333  # nolint
+  ## [5,]  2 0.8 0.6666667  # nolint
+  ## [6,]  2 1.0 1.0000000  # nolint
+  m <- matrix(c(
+      rep(1, 6), rep(2, 6),
+      rep(seq(0,1,0.2),2),
+      rep(0.5, 3), rep(1.0, 3), rep(2 / 9, 3), 1 / 3, 2 / 3, 1.0
+    ),
+    ncol = 3, byrow = FALSE)
   expected <- as.data.frame(x = m)
-  colnames(expected) <- c("time", paste(rep("N", 2),
-    as.character(seq(1, n_phylogenies)), sep = ""))
+  colnames(expected) <- c("ID", "t", "nltt")
+  expected[, 1] <- sapply(expected[, 1], as.integer)
 
+  expect_equal(nrow(result), 12)
+  expect_equal(ncol(result), 3)
   expect_equal(all.equal(result, expected), TRUE)
+})
+
+
+
+test_that(paste("get_nltt_values: ",
+  "How to stretch an nltt timepoints matrix: ",
+  "Example: Three harder trees", sep = ""), {
+
+  newick1 <- "((A:1,B:1):1,(C:1,D:1):1);"
+  newick2 <- paste("((((XD:1,ZD:1):1,CE:2):1,(FE:2,EE:2):1):4,",
+    "((AE:1,BE:1):1,(WD:1,YD:1):1):5);", sep = "")
+  newick3 <- ape::rcoal(30)
+  phylogeny1 <- ape::read.tree(text = newick1)
+  phylogeny2 <- ape::read.tree(text = newick2)
+  phylogeny3 <- ape::read.tree(text = newick2)
+  phylogenies <- c(phylogeny1, phylogeny2, phylogeny3)
+
+  # The real tests
+  result <- ribir::get_nltt_values(phylogenies, dt = 0.20)
+
+  expect_equal(nrow(result), 18)
+  expect_equal(ncol(result), 3)
 })
