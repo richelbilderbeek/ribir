fca9c430e367698addfaed1f06a7066fa618b874
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-30 16:27:30 +0200

    Improved docs, shortened test

--- tests/testthat/test_lintr.R
+++ tests/testthat/test_lintr.R
@@ -1,9 +1,4 @@
 test_that("Package must pass lintr", {
   skip("Only at a new release")
-  if (requireNamespace("lintr", quietly = TRUE)) {
-    context("lints")
-    test_that("Package Style", {
-      lintr::expect_lint_free()
-    })
-  }
+  lintr::expect_lint_free()
 })
