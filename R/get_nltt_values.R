fd0501623bd420deba05a5eef2bc112400e19016
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-04 10:52:42 +0200

    Improved example code

--- R/get_nltt_values.R
+++ R/get_nltt_values.R
@@ -24,9 +24,9 @@
 #'   nltt_values <- get_nltt_values(phylogenies, dt = 0.2)
 #'
 #'   # Plot the phylognies, where the individual nLTT values are visible
-#'   qplot(
-#'     t, nltt, data = nltt_values, geom = "blank", ylim = c(0,1),
-#'     main = "Average nLTT plot of phylogenies"
+#'   qplot(t, nltt, data = nltt_values, geom = "point",
+#'     ylim = c(0,1),
+#'     main = "Average nLTT plot of phylogenies", color = id, size = I(0.1)
 #'   ) + stat_summary(
 #'     fun.data = "mean_cl_boot", color = "red", geom = "smooth"
 #'   )
