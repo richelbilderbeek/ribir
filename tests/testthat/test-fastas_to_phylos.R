8ed4254ae471de640d5599a03633ee0d71ad7b43
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-15 13:59:46 +0100

    Fix tests

--- tests/testthat/test-fastas_to_phylos.R
+++ tests/testthat/test-fastas_to_phylos.R
@@ -1,36 +1,34 @@
-context("fastas_to_phylos")
-
 test_that("use, one FASTA filename", {
 
   fasta_filename <- beautier::get_fasta_filename()
   crown_age <- 25.0
-  phylos <- beautier::fastas_to_phylos(
+  phylos <- fastas_to_phylos(
     fasta_filenames = fasta_filename,
     crown_age = crown_age
   )
-  testthat::expect_equal(length(phylos), 1)
+  expect_equal(length(phylos), 1)
 })
 
 test_that("use, two FASTA filenames", {
 
-  fasta_filenames <- get_beautier_paths(c("anthus_aco.fas", "anthus_nd2.fas"))
+  fasta_filenames <- beautier::get_beautier_paths(c("anthus_aco.fas", "anthus_nd2.fas"))
   crown_age <- 36.0
-  phylos <- beautier::fastas_to_phylos(
+  phylos <- fastas_to_phylos(
     fasta_filenames = fasta_filenames,
     crown_age = crown_age
   )
-  testthat::expect_equal(length(phylos), 2)
+  expect_equal(length(phylos), 2)
 })
 
 test_that("abuse", {
 
   testthat::expect_error(
-    beautier::fastas_to_phylos(fasta_filenames = "absent", crown_age = 15),
+    fastas_to_phylos(fasta_filenames = "absent", crown_age = 15),
     "'fasta_filenames' must be the names of existing files"
   )
 
   testthat::expect_error(
-    beautier::fastas_to_phylos(
+    fastas_to_phylos(
       fasta_filenames = beautier::get_fasta_filename(),
       crown_age = -42
     ),
